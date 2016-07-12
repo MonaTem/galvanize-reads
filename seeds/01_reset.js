6517bbae8bd8083d144bd273d5eb7361b2230708
Christopher Comella <git.chrisco@gmail.com>
2016-07-12 16:09:00 -0600

    Deliver migrations and seeds for all DB tables

---
+++ seeds/01_reset.js
@@ -0,0 +1,7 @@
+exports.seed = function(knex, Promise) {
+	return Promise.all([
+		knex.raw('TRUNCATE book_author RESTART IDENTITY CASCADE'),
+		knex.raw('TRUNCATE book RESTART IDENTITY CASCADE'),
+		knex.raw('TRUNCATE author RESTART IDENTITY CASCADE')
+	]);
+};
